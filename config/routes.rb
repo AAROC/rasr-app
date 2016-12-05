750279670c84a117a3061936c840b0dd6afebaaf
Bruce Becker <brucellino@gmail.com>
2016-12-05 15:24:15 +0100

    Removed the rasr controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  get 'rasr/start'
-
-  get 'rasr/index'
-
   # ORCID
   # Match is deprecated
   # match '/auth/:provider/callback' => 'authentications#orcid'
@@ -26,4 +22,5 @@ Rails.application.routes.draw do
   resources :experiments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
 end
--- test/controllers/rasr_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RasrControllerTest < ActionDispatch::IntegrationTest
-  test "should get start" do
-    get rasr_start_url
-    assert_response :success
-  end
-
-end
